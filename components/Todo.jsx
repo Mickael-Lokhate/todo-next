0fff27ce983129b68f1162defee2c4f527bbf31d
Mickael Lokhate <mlokhate@student.42nice.fr>
2022-05-27 07:52:57 +0200

    UI Improvments + check todos + sort todos

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,23 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/fontawesome-free-solid";
 
-function Todo(props) {
-  const todo = props.todo;
+function Todo({ todo, handleChecked }) {
   return (
     <>
-      <div className="todo">
+      <div
+        className={`todo checked-${todo.checked}`}
+        onClick={() => handleChecked(todo.id)}
+      >
+        {todo.checked ? <div className="div-checked"></div> : false}
         <div className="todo-infos">
           <h3>{todo.title}</h3>
           <p>{todo.desc}</p>
         </div>
 
-        {todo.checked ? <FontAwesomeIcon icon={faCheck} /> : false}
+        {todo.checked ? <FontAwesomeIcon icon={faCheck} size={"lg"} /> : false}
+
         <style jsx>{`
           .todo {
             display: flex;
             flex-direction: row;
             justify-content: space-between;
             align-items: center;
+            position: relative;
+            box-shadow: 2px 2px 5px #a1a1a1;
+            margin-bottom: 10px;
+            padding: 10px;
+            min-height: 100px;
+            background-color: #a1a1a1;
+          }
+
+          .checked-false {
+            transition: all 1s;
+          }
+
+          .checked-false:hover {
+            opacity: 0.8;
+            box-shadow: none;
+          }
+
+          .div-checked {
+            position: absolute;
+            top: 50%;
+            height: 2px;
+            width: 90%;
+            background-color: #565656;
+            display: flex;
+            justify-content: center;
+            margin: 0 auto;
+            align-items: center;
+            left: 5%;
           }
 
           .todo:hover {
@@ -27,10 +59,21 @@ function Todo(props) {
           .todo-infos {
             display: flex;
             flex-direction: column;
+            justify-content: center;
+            margin: 0;
+          }
+
+          .todo-infos p,
+          .todo-infos h3 {
+            margin: 0;
+          }
+
+          .checked-true {
+            opacity: 0.5;
+            box-shadow: none;
           }
         `}</style>
       </div>
-      <hr />
     </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 import Todo from "../components/Todo";
 
 function Todos({ todos }) {
+  const [allTodos, setTodos] = useState(todos);
+
+  const handleChecked = (id) => {
+    console.log(`CLICK ${id}`);
+    const newArray = [...allTodos];
+    if (newArray.find((t) => t.id === id).checked)
+      newArray.find((t) => t.id === id).checked = false;
+    else newArray.find((t) => t.id === id).checked = true;
+
+    newArray.sort((a, b) => {
+      if (a.checked) return 1;
+      else if (b.checked) return -1;
+      else return 0;
+    });
+
+    setTodos(newArray);
+  };
+
+  useEffect(() => {}, [allTodos]);
+
   return (
     <Layout title="To-do List">
       <div className="todos-container">
-        {todos.map((t, i) => (
-          <Todo key={i} todo={t} />
+        {allTodos.map((t, i) => (
+          <Todo key={i} todo={t} handleChecked={handleChecked} />
         ))}
 
         <style jsx>{`
@@ -30,7 +51,11 @@ Todos.getInitialProps = async () => {
     { id: 2, title: "Test 3", desc: "My third todo", checked: true },
     { id: 3, title: "Test 4", desc: "My fourth todo", checked: false },
   ];
-
+  todos.sort((a, b) => {
+    if (a.checked) return 1;
+    else if (b.checked) return -1;
+    return 0;
+  });
   return {
     todos,
   };
