da6f63064e46b4e39ea8498c8b26011183793ab6
Mickael Lokhate <mlokhate@student.42nice.fr>
2022-06-03 11:18:37 +0200

    Functionnal floating button + Add the categories and todos adding forms + update the list real time

--- pages/category/[id].jsx
+++ pages/category/[id].jsx
@@ -1,11 +1,84 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import FloatButton from "../../components/FloatButton";
 import Layout from "../../components/Layout";
 import Todo from "../../components/Todo";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function AddTodoForm({ modal, cat_id, setTodos }) {
+  const [formValues, setFormValues] = useState({
+    title: "",
+    desc: "",
+  });
+
+  const handleChange = (e, name) => {
+    const val = e.target.value;
+    setFormValues({ ...formValues, [name]: val });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const query = `mutation AddTodo($title: String!, $cat_id: Int!, $desc: String) {
+      addTodo(title: $title, cat_id: $cat_id, desc: $desc) {
+        id,
+        title,
+        checked,
+        desc
+      }
+    }`;
+    const res = await fetch("http://localhost:3001/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables: { title: formValues.title, cat_id, desc: formValues.desc },
+      }),
+    });
+    const data = (await res.json()).data;
+    if (data) {
+      setTodos(data.addTodo);
+      modal(false);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="todoTitle">
+        <Form.Label>To-Do Title</Form.Label>
+        <Form.Control
+          required
+          type="text"
+          placeholder="Buy bread, Call boss, ..."
+          value={formValues.title}
+          onChange={(e) => handleChange(e, "title")}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="todoDesc">
+        <Form.Label>To-Do Description</Form.Label>
+        <Form.Control
+          required
+          type="text"
+          placeholder="More details..."
+          value={formValues.desc}
+          onChange={(e) => handleChange(e, "desc")}
+        ></Form.Control>
+      </Form.Group>
+      <Button type="submit" variant="secondary">
+        Save
+      </Button>
+    </Form>
+  );
+}
 
 function Todos({ todos, category }) {
   const router = useRouter();
   const [allTodos, setTodos] = useState(todos);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const handleChecked = async (id) => {
     const query = `mutation CheckTodo($id: Int!) {
@@ -33,14 +106,26 @@ function Todos({ todos, category }) {
     setTodos([...newTodos, data]);
   };
 
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <Layout title="To-do List">
+    <Layout title={category.name}>
       <div className="blur-bg"></div>
       <div className="todos-container">
         {allTodos.map((t, i) => (
           <Todo key={i} todo={t} handleChecked={handleChecked} />
         ))}
       </div>
+      <FloatButton
+        color={"#563d7c"}
+        title="Add a new to-do"
+        CustomOnClick={openModal}
+      />
       <style jsx>{`
         .todos-container {
           width: 50%;
@@ -60,6 +145,19 @@ function Todos({ todos, category }) {
           filter: blur(200px);
         }
       `}</style>
+
+      <Modal centered show={modalOpen} onHide={closeModal}>
+        <Modal.Header>
+          <Modal.Title>Add a To-Do</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddTodoForm
+            modal={setModalOpen}
+            cat_id={category.id}
+            setTodos={setTodos}
+          />
+        </Modal.Body>
+      </Modal>
     </Layout>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,81 @@
 import Layout from "../components/Layout";
 import Category from "../components/Category";
 import FloatButton from "../components/FloatButton";
-import React from "react";
-import Modal from "react-modal";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function AddCategoryForm({ modal, setCategories }) {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    color: "#563d7c",
+  });
+
+  const handleChange = (e, name) => {
+    const val = e.target.value;
+    setFormValues({ ...formValues, [name]: val });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const query = `mutation AddCategory($name: String!, $color: String!) {
+        addCategory(name: $name, color: $color) {
+          id,
+          name,
+          color
+        }
+    }`;
+    const res = await fetch("http://localhost:3001/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables: { name: formValues.name, color: formValues.color },
+      }),
+    });
+    const data = (await res.json()).data;
+    setCategories(data.addCategory);
+    modal(false);
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="catName">
+        <Form.Label>Category name</Form.Label>
+        <Form.Control
+          required
+          type="text"
+          placeholder="Work, Home, Project, ..."
+          value={formValues.name}
+          onChange={(e) => handleChange(e, "name")}
+        ></Form.Control>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="catColor">
+        <Form.Label>Choose a color</Form.Label>
+        <Form.Control
+          required
+          type="color"
+          title="Choose a background color"
+          value={formValues.color}
+          onChange={(e) => handleChange(e, "color")}
+        ></Form.Control>
+      </Form.Group>
+      <Button type="submit">Save</Button>
+    </Form>
+  );
+}
 
 function Home({ categories }) {
-  const [isModalOpen, setModalOpen] = React.useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [cat, setCategories] = useState(categories);
 
   const handleClickCategory = () => {
     setModalOpen(true);
   };
-
   const onCloseModal = () => {
     setModalOpen(false);
   };
@@ -18,7 +83,7 @@ function Home({ categories }) {
   return (
     <Layout title={"Home"}>
       <div className="categories">
-        {categories.map((c, i) => (
+        {cat.map((c, i) => (
           <Category category={c} key={i} />
         ))}
       </div>
@@ -33,12 +98,13 @@ function Home({ categories }) {
         }
       `}</style>
 
-      <Modal
-        onRequestClose={onCloseModal}
-        isOpen={isModalOpen}
-        contentLabel="test"
-      >
-        <h2>Add a Category</h2>
+      <Modal show={isModalOpen} onHide={onCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Add a category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddCategoryForm modal={setModalOpen} setCategories={setCategories} />
+        </Modal.Body>
       </Modal>
       <FloatButton
         color={"#12aadd"}
